8cd79dee18e7bc1ff6d37ef084b87dcbd17bba01
Neil Johnson <nj.designs@protomail.com>
2020-05-23 08:06:39 +0100

    End of Part 43

---
+++ Scripts/PausePanel.cs
@@ -0,0 +1,29 @@
+using Godot;
+using System;
+
+public class PausePanel : BaseMenuPanel
+{
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        base._Ready();
+    }
+
+    public void OnSigQuitButton()
+    {
+        // GetTree().ChangeScene("res://Scenes/GameMenu.tscn");
+        GetTree().Quit();
+    }
+
+    public void OnSigContinueButton()
+    {
+        GetTree().Paused = false;
+        SlideOut();
+    }
+
+    public void OnSigPauseGame()
+    {
+        SlideIn();
+    }
+}
